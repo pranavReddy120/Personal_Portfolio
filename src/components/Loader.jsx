ef171a277c172b3343c6b7bd08d8d8d235f441b3
Pranav <pranavbudlu@gmail.com>
2025-01-27 17:46:43 -0500

    uploading 3D model section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Html, useProgress } from '@react-three/drei'; 
 
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
-  )
+    <Html center>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh', // Full viewport height
+        textAlign: 'center',
+      }}>
+        <span className="canvas-loader"></span>
+        <p style={{ 
+          fontSize: 14, 
+          color: '#f1f1f1', 
+          fontWeight: 800, 
+          marginTop: 40 
+        }}>
+          Rendering:&nbsp;
+          <span style={{ display: 'inline' }}>{progress.toFixed(2)}%</span>
+        </p>
+      </div>
+    </Html>
+  );
 }
 
-export default Loader
+export default Loader;
